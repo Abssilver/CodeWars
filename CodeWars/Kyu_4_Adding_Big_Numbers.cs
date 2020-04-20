6d462f685d996ce7f05441af823d22d2f23eda9d
Pavel <zeroblaze@yandex.ru>
2020-04-20 22:45:33 +0300

    4 kyu Adding Big Numbers

--- CodeWars/Kyu_4_Adding_Big_Numbers.cs
+++ CodeWars/Kyu_4_Adding_Big_Numbers.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
+/*
+We need to sum big numbers and we require your help.
+
+Write a function that returns the sum of two numbers.
+The input numbers are strings and the function must return a string.
+
+Example
+add("123", "321"); -> "444"
+add("11", "99");   -> "110"
+Notes
+The input numbers are big.
+The input is a string of only digits
+The numbers are positives
+*/
 namespace CodeWars
 {
     class Kyu_4_Adding_Big_Numbers
