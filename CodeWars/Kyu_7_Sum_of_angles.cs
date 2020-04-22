df42623d2d90053a3f192b1ffbaaad2e18a0066a
Pavel <zeroblaze@yandex.ru>
2020-04-22 12:31:31 +0300

    7 kyu Sum of angles

---
+++ CodeWars/Kyu_7_Sum_of_angles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*
+Find the total sum of internal angles in an n-sided simple polygon. 
+N will be greater than 2.
+*/
+
+namespace CodeWars
+{
+    class Kyu_7_Sum_of_angles
+    {
+        public int Angle(int n) => (n - 2) * 180;
+    }
+}
