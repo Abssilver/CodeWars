a7f6ba40bb997b41ff455454ed22839f0396ec42
Pavel <zeroblaze@yandex.ru>
2020-04-22 12:34:53 +0300

    8 kyu Multiply

---
+++ CodeWars/Kyu_8_Multiply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+The code does not execute properly. 
+Try to figure out why.
+*/
+
+namespace CodeWars
+{
+    class Kyu_8_Multiply
+    {
+        public int multiply(int a, int b) => a * b;
+    }
+}
